7940df2b017c37b6522eae22882ebe3e1c6bf68b
leonredman <leonproart@yahoo.com>
2020-03-07 22:34:48 -0700

    init commit

--- assets/js/craigslist/pages/Details.js
+++ assets/js/craigslist/pages/Details.js
@@ -12,8 +12,29 @@ export default class Details extends Component {
     const {match, location, history} = this.props
     return (
       <div className="details-page">
-      <div className="container">
-        this item is {match.params.item}
+        <div className="container">
+          <section className="submenu">
+          <div className=direction>
+            <div className="prev">Prev</div>
+            <div className="next">Next</div>
+          </div>
+
+          <nav className="sub-links">
+            <a href="#">More Ads by User</a>
+            <a href="#">Print</a>
+            <a href="#">Share</a>
+            <a href="#">Contact Seller</a>
+          </nav>
+          </section>
+
+          <section>
+          <div className="media-column">
+
+          </div>
+
+          <div className="details-column"></div>
+
+          </section>
 
     </div>
     </div>
